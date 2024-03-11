36bc164fad259ac7339c814397c7876430a30ecc
Taya <tayaibragimova@gmail.com>
2024-03-11 23:50:06 +0300

    contest5

--- contests/contest_5/problem3_complex.cpp
+++ contests/contest_5/problem3_complex.cpp
@@ -1,96 +1,148 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
-class Complex {
-    private:
-        double re;
-        double im;
+class Complex
+{
+private:
+    double re;
+    double im;
 
-    public:
-        Complex (double, double=0); //чтобы не прописывать  Default constructor, неявно преобразует остальное в Complex
-        Complex operator+ (const Complex&);
-        Complex operator- (const Complex&);
-        Complex operator* (const Complex&);
-        Complex operator/ (const Complex&);
+public:
+    Complex(double, double = 0);//чтобы не прописывать  Default constructor, неявно преобразует остальное в Complex
+    Complex operator+(const Complex&) const;
+    Complex operator+(double) const;
 
-        bool operator== (const Complex&);
-        bool operator!= (const Complex&);
+    Complex operator-(const Complex&) const;
+    Complex operator-(double) const;
 
-        Complex operator+ ();
-        Complex operator- ();
+    Complex operator*(const Complex&) const;
+    Complex operator*(double) const;
 
-        double abs();
-        double Re();
-        double Im();
+    Complex operator/(const Complex&) const;
 
-    friend std::ostream& operator<< (std::ostream& ss, const Complex& comp);
+    bool operator==(const Complex&) const;
+    bool operator!=(const Complex&) const;
 
+    Complex operator+();
+    Complex operator-();
+
+    double Re() const;
+    double Im() const;
+
+    friend std::ostream& operator<<(std::ostream& ss, const Complex& comp);
 };
 
-Complex::Complex(double re, double im) {
+Complex::Complex(double re, double im)
+{
     this->re = re;
     this->im = im;
 }
 
-std::ostream& operator<<(std::ostream& ss, const Complex& comp){
-    if (comp.im == 0) {
-        ss << comp.re; 
-    } else if (comp.im < 0) {
+std::ostream& operator<<(std::ostream& ss, const Complex& comp)
+{
+    if (comp.im == 0)
+    {
+        ss << comp.re;
+    }
+    else if (comp.im < 0)
+    {
         ss << comp.re << comp.im << "i";
-    } else {
+    }
+    else
+    {
         ss << comp.re << "+" << comp.im << "i";
     }
     return ss;
 }
 
-Complex Complex::operator+(const Complex& other) {
-    return Complex {re + other.re, im + other.im};
+Complex Complex::operator+(const Complex& other) const
+{
+    return Complex{re + other.re, im + other.im};
 }
 
-Complex Complex::operator-(const Complex& other) {
-    return Complex {re - other.re, im - other.im};
+Complex Complex::operator+(double right) const
+{
+    return *this + Complex{right};
+};
+
+Complex operator+(double left, const Complex& right)
+{
+    return right + left;
 }
 
-Complex Complex::operator*(const Complex& other) {
-    return Complex {re * other.re - im * other.im, re * other.im + im * other.re};
+Complex Complex::operator-(const Complex& other) const
+{
+    return Complex{re - other.re, im - other.im};
 }
 
-Complex Complex::operator/(const Complex& other) {
-    return Complex {(re * other.re + im * other.im)/(std::pow(other.re, 2) + std::pow(other.im, 2)), 
-    (im * other.re - re * other.im)/(std::pow(other.re, 2) + std::pow(other.im, 2))};
+Complex Complex::operator-(double right) const
+{
+    return *this - Complex{right};
+};
+
+Complex operator-(double left, const Complex& right)
+{
+    return -(right - left);
 }
 
-bool Complex::operator==(const Complex& other) {
+Complex Complex::operator*(const Complex& other) const
+{
+    return Complex{re * other.re - im * other.im, re * other.im + im * other.re};
+}
+
+Complex Complex::operator*(double right) const
+{
+    return (*this) * Complex{right};
+}
+
+Complex operator*(double left, const Complex& other)
+{
+    return other * left;
+}
+
+Complex Complex::operator/(const Complex& other) const
+{
+    return Complex{(re * other.re + im * other.im) / (std::pow(other.re, 2) + std::pow(other.im, 2)),
+                   (im * other.re - re * other.im) / (std::pow(other.re, 2) + std::pow(other.im, 2))};
+}
+
+bool Complex::operator==(const Complex& other) const
+{
     return (re == other.re && im == other.im);
 }
-bool Complex::operator!=(const Complex& other) {
+bool Complex::operator!=(const Complex& other) const
+{
     return (re != other.re || im != other.im);
 }
 
-double Complex::abs() 
+double abs(const Complex& comp)
 {
-    return std::pow(std::pow(re, 2) + std::pow(im, 2), 0.5);
+    return std::pow(std::pow(comp.Re(), 2) + std::pow(comp.Im(), 2), 0.5);
 }
 
-Complex Complex::operator+() 
+Complex Complex::operator+()
 {
-    return Complex {+re, +im};
+    return Complex{+re, +im};
 }
 
-Complex Complex::operator-() {
-    return Complex {-re, -im};
+Complex Complex::operator-()
+{
+    return Complex{-re, -im};
 }
 
-double Complex::Re() {
+double Complex::Re() const
+{
     return this->re;
 }
 
-double Complex::Im() {
+double Complex::Im() const
+{
     return this->im;
 }
 
-int main() {
-    Complex comp1 {1, 1};
-    Complex comp2 {2};
-    std::cout << comp1.Im();
+int main()
+{
+    Complex comp1{1, 1};
+    Complex comp2{2, 2};
+    // std::cout << comp2.abs();
 }
--- contests/contest_5/problem4_rational.cpp
+++ contests/contest_5/problem4_rational.cpp
@@ -1,160 +1,208 @@
 #include <iostream>
 #include <numeric>
 
-class Rational {
+class Rational
+{
 private:
-  int num;
-  int den;
+    int num;
+    int den;
 
 public:
-  Rational(int = 0, int = 1);
-  Rational operator+(const Rational &);
-  Rational operator+=(const Rational &);
-  Rational operator-=(const Rational &);
-  Rational operator*=(const Rational &);
-  Rational operator/=(const Rational &);
+    Rational(int = 0, int = 1);
+    Rational operator+(const Rational&);
+    Rational operator+=(const Rational&);
+    Rational operator-=(const Rational&);
+    Rational operator*=(const Rational&);
+    Rational operator/=(const Rational&);
 
-  Rational operator++();
-  Rational operator--();
-  Rational operator++(int);
-  Rational operator--(int);
+    Rational operator++();
+    Rational operator--();
+    Rational operator++(int);
+    Rational operator--(int);
 
-  Rational operator-(const Rational &);
-  Rational operator*(const Rational &);
-  Rational operator/(const Rational &);
+    Rational operator-(const Rational&);
+    Rational operator*(const Rational&);
+    Rational operator/(const Rational&);
 
-  bool operator==(const Rational &);
-  bool operator!=(const Rational &);
+    bool operator==(const Rational&);
+    bool operator!=(const Rational&);
 
-  Rational operator+();
-  Rational operator-();
+    Rational operator+();
+    Rational operator-();
 
-  Rational normalize();
-  double Num();
-  double Den();
 
-  friend std::ostream &operator<<(std::ostream &ss, const Rational &comp);
+    Rational normalize() const;
+    double numerator() const;
+    double denominator() const;
+
+    friend std::ostream& operator<<(std::ostream& ss, const Rational& comp);
 };
 
-Rational::Rational(int num, int den) {
-  this->num = num;
-  this->den = den;
+
+Rational::Rational(int num, int den)
+{
+    this->num = num;
+    this->den = den;
 }
 
-double Rational::Num() {
-  Rational r = this->normalize();
-  return r.num;
+double Rational::numerator() const
+{
+    Rational r = this->normalize();
+    return r.num;
 }
 
-double Rational::Den() {
-  Rational r = this->normalize();
-  return r.den;
+double Rational::denominator() const
+{
+    Rational r = this->normalize();
+    return r.den;
 }
 
-// unary -
-Rational Rational::operator-() { return {-num, den}; }
-
-// unary +
-Rational Rational::operator+() { return {+num, den}; }
-
-Rational Rational::operator+(const Rational &other) {
-  int comDen = std::lcm(den, other.den);
-  return {((num * comDen / den) + (other.num * comDen / other.den)), comDen};
+//unary -
+Rational Rational::operator-()
+{
+    return {-num, den};
 }
 
-Rational Rational::operator+=(const Rational &other) {
-  *this = *this + other;
-  return *this;
+//unary +
+Rational Rational::operator+()
+{
+    return {+num, den};
+}
+
+
+Rational Rational::operator+(const Rational& other)
+{
+    int comDen = std::lcm(den, other.den);
+    return {((num * comDen / den) + (other.num * comDen / other.den)), comDen};
+}
+
+Rational Rational::operator+=(const Rational& other)
+{
+    *this = *this + other;
+    return *this;
 }
 
 // int + rational
-Rational operator+(int num, Rational &rational) { return rational + num; }
-
-Rational Rational::operator-(const Rational &other) {
-  int comDen = std::lcm(den, other.den);
-  return {((num * comDen / den) - (other.num * comDen / other.den)), comDen};
+Rational operator+(int num, Rational& rational)
+{
+    return rational + num;
 }
 
-Rational Rational::operator-=(const Rational &other) {
-  *this = *this - other;
-  return *this;
+
+Rational Rational::operator-(const Rational& other)
+{
+    int comDen = std::lcm(den, other.den);
+    return {((num * comDen / den) - (other.num * comDen / other.den)), comDen};
+}
+
+Rational Rational::operator-=(const Rational& other)
+{
+    *this = *this - other;
+    return *this;
 }
 
 // int - rational
-Rational operator-(int num, Rational &rational) { return (-(rational - num)); }
-
-Rational Rational::operator*(const Rational &other) {
-  return {num * other.num, den * other.den};
+Rational operator-(int num, Rational& rational)
+{
+    return (-(rational - num));
 }
 
-Rational Rational::operator*=(const Rational &other) {
-  *this = *this * other;
-  return *this;
+
+Rational Rational::operator*(const Rational& other)
+{
+    return {num * other.num, den * other.den};
+}
+
+Rational Rational::operator*=(const Rational& other)
+{
+    *this = *this * other;
+    return *this;
 }
 
 // int * rational
-Rational operator*(int i, Rational &other) { return (other * i); }
-
-Rational Rational::operator/(const Rational &other) {
-  return {num * other.den, den * other.num};
+Rational operator*(int i, Rational& other)
+{
+    return (other * i);
 }
 
-Rational Rational::operator/=(const Rational &other) {
-  *this = *this / other;
-  return *this;
+
+Rational Rational::operator/(const Rational& other)
+{
+    return {num * other.den, den * other.num};
+}
+
+Rational Rational::operator/=(const Rational& other)
+{
+    *this = *this / other;
+    return *this;
 }
 
 // int / rational
-Rational operator/(int i, Rational &other) { return (Rational{i, 1} / other); }
-
-Rational Rational::normalize() {
-  int gcd = std::gcd(num, den);
-  return {num / gcd, den / gcd};
+Rational operator/(int i, Rational& other)
+{
+    return (Rational{i, 1} / other);
 }
 
-bool Rational::operator==(const Rational &other) {
-  Rational r = other;
-  return (this->normalize().num == (r.normalize()).num) &&
-         (this->normalize().den == (r.normalize()).den);
-}
-bool Rational::operator!=(const Rational &other) {
-  Rational r = other;
-  return (this->normalize().num != (r.normalize()).num) ||
-         (this->normalize().den != (r.normalize()).den);
+
+Rational Rational::normalize() const
+{
+    int gcd = std::gcd(num, den);
+    return {num / gcd, den / gcd};
 }
 
-Rational Rational::operator++() {
-  *this = *this + 1;
-  return *this;
+
+bool Rational::operator==(const Rational& other)
+{
+    Rational r = other;
+    return (this->normalize().num == (r.normalize()).num) &&
+           (this->normalize().den == (r.normalize()).den);
+}
+bool Rational::operator!=(const Rational& other)
+{
+    Rational r = other;
+    return (this->normalize().num != (r.normalize()).num) ||
+           (this->normalize().den != (r.normalize()).den);
 }
 
-Rational Rational::operator++(int) {
-  *this = *this + 1;
-  return *this;
+Rational Rational::operator++()
+{
+    *this = *this + 1;
+    return (*this).normalize();
 }
 
-Rational Rational::operator--() {
-  *this = *this - 1;
-  return *this;
+Rational Rational::operator++(int)
+{
+    Rational r = *this;
+    // *this = *this + 1;
+    ++(*this);
+    return r.normalize();
 }
 
-Rational Rational::operator--(int) {
-  *this = *this - 1;
-  return *this;
+Rational Rational::operator--()
+{
+    *this = *this - 1;
+
+    return (*this).normalize();
 }
 
-std::ostream &operator<<(std::ostream &ss, const Rational &r) {
-  if (r.num == 0) {
-    ss << 0;
-  } else {
-    ss << r.num << "/" << r.den;
-  }
-  return ss;
+Rational Rational::operator--(int)
+{
+    Rational r = *this;
+    // *this = *this - 1;
+    ++(*this);
+
+    return r.normalize();
 }
 
-int main() {
-  Rational r{1, 4};
-  Rational p{1, 4};
-  // std::cout << (p - 1);
-  std::cout << (--p);
+std::ostream& operator<<(std::ostream& ss, const Rational& r)
+{
+    if (r.num == 0)
+    {
+        ss << 0;
+    }
+    else
+    {
+        ss << r.num << "/" << r.den;
+    }
+    return ss;
 }
